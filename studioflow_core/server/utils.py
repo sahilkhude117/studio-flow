19c40717adddff1fb108647113fd20ff07c60ed5
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-26 12:43:09 +0530

    util: send_from_dist

---
+++ studioflow_core/server/utils.py
@@ -0,0 +1,28 @@
+import inspect
+import mimetypes
+import typing
+from pathlib import Path
+
+import flask
+import werkzeug.exceptions as wz_ex
+
+import studioflow_statics
+
+dist_folder = Path(inspect.getfile(studioflow_statics)).joinpath("../dist").resolve()
+
+def send_from_dist(
+    filename: str,
+    fallback: typing.Optional[str] = None,
+    dist_folder: Path = dist_folder,
+):
+    mimetypes.add_type("application/javascript", ".js")
+    mimetypes.add_type("text/css", ".css")
+    mimetypes.add_type("text/html", ".html")
+
+    try: 
+        return flask.send_from_directory(dist_folder, filename)
+    except wz_ex.NotFound:
+        if fallback is None:
+            return flask.Response(status=404)
+        return flask.send_from_directory(dist_folder, fallback)
+
