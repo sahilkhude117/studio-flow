1af9cbecf7e916a55eacc048e19ff9388b53b554
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-27 15:56:24 +0530

    main: users repo

--- studioflow_core/controllers/main.py
+++ studioflow_core/controllers/main.py
@@ -7,7 +7,16 @@ from typing import Any, Dict, List, Optional, Tuple
 import flask
 
 from studioflow_core.repositories.keyvalue import KVRepository
+from studioflow_core.repositories.jwt_signer import EditorJWTRepository, JWTRepository
+from studioflow_core.repositories.email import EmailRepository
+from studioflow_core.repositories.tasks import TasksRepository
+from studioflow_core.repositories.users import UsersRepository
 
 class MainController:
     kv_repository: KVRepository
+    jwt_repository: JWTRepository
+    email_repository: EmailRepository
+    tasks_repository: TasksRepository
+    users_repository: UsersRepository
+
   
