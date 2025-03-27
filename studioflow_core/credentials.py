9ec15c1e0875f6cbaa7411dc4726de254d914487
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-27 15:41:23 +0530

    cred: resolve_headers

--- studioflow_core/credentials.py
+++ studioflow_core/credentials.py
@@ -12,3 +12,23 @@ def get_credentials():
         return None
 
     return credentials_path.read_text(encoding="utf-8").strip()
+
+def delete_credentials():
+    credentials_path = Settings.root_path.joinpath(CREDENTIALS_FILE)
+
+    if credentials_path.exists():
+        credentials_path.unlink()
+
+def set_credentials(token: str):
+    credentials_path = Settings.root_path.joinpath(CREDENTIALS_FILE)
+    credentials_path.parent.mkdir(exist_ok=True)
+
+    credentials_path.write_text(token, encoding="utf-8")
+
+def resolve_headers():
+    credentials = get_credentials()
+
+    if not credentials:
+        return None
+
+    return {"Api-Authorization": f"Bearer {credentials}"}
