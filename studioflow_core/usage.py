5e50ecaf2c19c38b2fda0e030fe4cb943113b1c8
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-30 18:22:43 +0530

    usage: added player functions

--- studioflow_core/usage.py
+++ studioflow_core/usage.py
@@ -8,11 +8,13 @@ import requests
 
 from studioflow_core.threaded import threaded
 from studioflow_core.credentials import get_credentials
-from studioflow_core.environment import CLOUD_API_CLI_URL
+from studioflow_core.jwt_auth import UserClaims, USER_AUTH_HEADER_KEY
+from studioflow_core.environment import CLOUD_API_CLI_URL, SIDECAR_HEADERS, SIDECAR_URL
 from studioflow_core.utils import get_local_python_version, get_local_user_id
 from studioflow_core.utils.env import is_dev_env, is_test_env
+from studioflow_core.utils.packages import get_local_package_version
 
-#EDITOR
+# EDITOR
 @threaded
 def send_editor_usage(payload: Dict):
     if is_test_env() or is_dev_env():
@@ -22,7 +24,7 @@ def send_editor_usage(payload: Dict):
         "payload": payload,
         "userId": get_local_user_id(),
         "pythonVersion": get_local_python_version(),
-        "studioflowVersion": "studioflow-0.0.0"
+        "abstraVersion": str(get_local_package_version()),
     }
 
     headers = {"apiKey": get_credentials()}
@@ -31,7 +33,6 @@ def send_editor_usage(payload: Dict):
 
 
 def editor_usage(func: Callable[..., Any]) -> Callable[..., Any]:
-
     @wraps(func)
     def wrapper(*args: Tuple[Any], **kwargs: Any) -> Any:
         arg_names = inspect.getfullargspec(func).args
@@ -45,3 +46,59 @@ def editor_usage(func: Callable[..., Any]) -> Callable[..., Any]:
 
 def editor_manual_usage(*, event: str, payload: Dict):
     send_editor_usage({**payload, "event": event})
+
+
+# PLAYER
+@threaded
+def send_player_usage(*, event: str, payload: Dict, auth: Optional[str] = None):
+    if SIDECAR_URL is None:
+        return
+
+    body = dict(event=event, payload=payload)
+    if auth is not None:
+        if claims := UserClaims.from_jwt(auth.split(" ")[1]):
+            body["email"] = claims.email
+
+    requests.post(f"{SIDECAR_URL}/usage/event", headers=SIDECAR_HEADERS, json=body)
+
+
+def player_usage(func: Callable[..., Any]) -> Callable[..., Any]:
+    @wraps(func)
+    def wrapper(*args: Tuple[Any], **kwargs: Any) -> Any:
+        auth = flask.request.headers.get(USER_AUTH_HEADER_KEY)
+        arg_names = inspect.getfullargspec(func).args
+        arg_values = dict(zip(arg_names, args))
+
+        send_player_usage(
+            auth=auth, event=func.__name__, payload={**arg_values, **kwargs}
+        )
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+# Executions
+def _send_execution_usage(file: Path, status: str, exception: Optional[Exception]):
+    if SIDECAR_URL or is_test_env() or is_dev_env():
+        return
+
+    try:
+        editor_manual_usage(
+            event="execution_exception",
+            payload=dict(file=str(file), status=status, exception=str(exception)),
+        )
+    except Exception:
+        pass
+
+
+Result = Tuple[Literal["finished", "abandoned", "failed"], Optional[Exception]]
+
+
+def execution_usage(func: Callable[[Path], Result]) -> Callable[[Path], Result]:
+    @wraps(func)
+    def wrapper(file: Path) -> Result:
+        status, exception = func(file)
+        _send_execution_usage(file, status, exception)
+        return status, exception
+
+    return wrapper
