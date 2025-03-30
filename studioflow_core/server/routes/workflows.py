efda6aac4a7b5431ef38fc47deeb6699cc2a63f3
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-30 13:33:35 +0530

    routes: workflows

--- studioflow_core/server/routes/workflows.py
+++ studioflow_core/server/routes/workflows.py
@@ -1,14 +1,17 @@
 import flask
 
 from studioflow_core.logger import StudioFlowLogger
+from studioflow_core.controllers.main import MainController
+from studioflow_core.controllers.workflows import WorkflowController
 
-def get_editor_bp():
+def get_editor_bp(main_controller: MainController):
     bp = flask.Blueprint("editor_workflows",__name__)
+    controller = WorkflowController(main_controller.repositories)
 
     @bp.get('/')
     def _load_workflow():
         try:
-            return "hello from editor bp"
+            return controller.get_workflow()
         except Exception as e:
             StudioFlowLogger.capture_exception(e)
             return str(e), 500
