abb12681b3864ca09276b1ec460c96bc4309e8a0
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-30 18:34:21 +0530

    email: templates

---
+++ studioflow_core/email_templates/__init__.py
@@ -0,0 +1,5 @@
+from . import authentication as authn_template
+from . import message as message_template
+from . import task_waiting as task_waiting_template
+
+__all__ = ["authn_template", "task_waiting_template", "message_template"]
