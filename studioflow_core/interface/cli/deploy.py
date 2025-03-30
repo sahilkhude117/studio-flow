90fce91ca1f8fde3b91c1a19b7d2b4a05ac72c97
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-30 13:35:25 +0530

    cli: deploy

---
+++ studioflow_core/interface/cli/deploy.py
@@ -0,0 +1,49 @@
+import pathlib
+import tempfile
+import uuid
+import zipfile
+
+import requests
+
+from studioflow_core.cloud_api import create_build, update_build
+from studioflow_core.credentials import resolve_headers
+from studioflow_core.logger import StudioFlowLogger
+from studioflow_core.settings import Settings
+from studioflow_core.utils.file import files_from_directory
+
+def _generate_zip_file() -> pathlib.Path:
+    root_path = Settings.root_path
+    zip_path = pathlib.Path(tempfile.gettempdir(), f"{uuid.uuid4()}.zip")
+
+    with zipfile.ZipFile(zip_path, "w") as zip_file:
+        for file in files_from_directory(root_path):
+            zip_file.write(file, file.relative_to(root_path))
+
+    return zip_path
+
+
+def _upload_file(url: str, file_path: pathlib.Path):
+    with file_path.open("rb") as f:
+        requests.put(url=url, data=f.read())
+
+def deploy():
+    headers = resolve_headers()
+    if not headers:
+        print(
+            "No project credentials found. Please run `abstra editor` and login via the web interface."
+        )
+        return
+
+    data = create_build(headers)
+
+    try:
+        zip_path = _generate_zip_file()
+        _upload_file(url=data.url, file_path=zip_path)
+        update_build(headers=headers, build_id=data.build_id)
+    except Exception as e:
+        update_build(
+            headers=headers, build_id=data.build_id, error="Failed to upload files"
+        )
+        print("Failed to deploy project", e)
+        StudioFlowLogger.capture_exception(e)
+        raise e
