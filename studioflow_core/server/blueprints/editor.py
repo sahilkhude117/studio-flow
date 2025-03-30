fab9df3f541f90a1ca6a3c28b720ff5b996d9da4
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-30 13:33:09 +0530

    blueprint: editor

--- studioflow_core/server/blueprints/editor.py
+++ studioflow_core/server/blueprints/editor.py
@@ -3,21 +3,22 @@ import flask
 from studioflow_core.usage import editor_usage
 from studioflow_core.server.utils import send_from_dist
 
+from studioflow_core.controllers.main import MainController
 from studioflow_core.server.routes import workflows as workflows_router
 
-def __get_api_bp():
+def __get_api_bp(controller: MainController):
     bp = flask.Blueprint("editor_api", __name__)
 
-    workflows_bp = workflows_router.get_editor_bp()
+    workflows_bp = workflows_router.get_editor_bp(controller)
     bp.register_blueprint(workflows_bp, url_prefix='/workflows')
 
     return bp
 
 
-def get_editor_bp():
+def get_editor_bp(controller: MainController):
     bp = flask.Blueprint("editor", __name__)
 
-    api_bp = __get_api_bp()
+    api_bp = __get_api_bp(controller)
     bp.register_blueprint(api_bp, url_prefix="/api")
 
     @bp.get("/")
