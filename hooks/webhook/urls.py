46270271552292e8f993b0774d1458b78af4e1e6
sahilkhude117 <sahilkhude11@gmail.com>
2025-04-05 10:37:04 +0530

    refactor: removed create-user

--- hooks/webhook/urls.py
+++ hooks/webhook/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('catch/<str:user_id>/<str:flow_id>', views.catch_hook, name='catch-hook'),
-    path('users', views.create_user, name='create-user'),
-    path('users/<int:user_id>', views.get_user , name='get-user'),
 ]
--- hooks/webhook/views.py
+++ hooks/webhook/views.py
@@ -39,24 +39,3 @@ def catch_hook(request, user_id, flow_id):
             {"error": str(e)}, 
             status=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
-
-
-@api_view(['POST'])
-def create_user(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET'])
-def get_user(request, user_id):
-
-    try:
-        user = User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-    
-    serializer = UserSerializer(user)
-    return Response(serializer.data)
