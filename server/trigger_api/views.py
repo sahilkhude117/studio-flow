21d841617a633f8880c2031d91dc68265307b9d9
sahilkhude117 <sahilkhude11@gmail.com>
2025-04-07 03:34:08 +0530

    view: available triggers view

--- server/trigger_api/views.py
+++ server/trigger_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from database.models import AvailableTrigger
+from .serializers import AvailableTriggerSerializer
+
+class AvailableTriggersView(APIView):
+    def get(self, request):
+        available_triggers = AvailableTrigger.objects.all()
+        serializer = AvailableTriggerSerializer(available_triggers, many=True)
+
+        return Response({
+            'availableTriggers': serializer.data
+        })
