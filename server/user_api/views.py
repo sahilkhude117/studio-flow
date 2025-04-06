42e67079d0b8de33bf5a5253f9cc3da57681ee00
sahilkhude117 <sahilkhude11@gmail.com>
2025-04-06 19:53:11 +0530

    views: signup view

---
+++ server/user_api/views.py
@@ -0,0 +1,35 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class SignupView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+
+        if not email:
+            return Response({"error": "Email is required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if User.objects.filter(email=email).exists():
+            return Response({"error": "User already exists"}, status=status.HTTP_403_FORBIDDEN)
+        
+        if 'username' not in request.data:
+            request.data['username'] = email
+
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
