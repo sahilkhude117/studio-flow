9f958bd2686330617afba38c998932c4d95baa66
sahilkhude117 <sahilkhude11@gmail.com>
2025-04-07 03:11:21 +0530

    url: /api/v1/flow/:flowId

--- server/flow_api/urls.py
+++ server/flow_api/urls.py
@@ -1,7 +1,8 @@
 # flow_api/urls.py
 from django.urls import path
-from .views import FlowView
+from .views import FlowView, FlowDetailView
 
 urlpatterns = [
-    path('', FlowView.as_view(), name='flow')
+    path('', FlowView.as_view(), name='flow'),
+    path('<uuid:flowId>/', FlowDetailView.as_view(), name='flow-detail')
 ]
