418a77939157115500bc821acef3fa8c9ec63f41
sahilkhude117 <sahilkhude11@gmail.com>
2025-04-06 21:07:09 +0530

    serializer: FlowListSerializer

--- server/flow_api/serializers.py
+++ server/flow_api/serializers.py
@@ -1,9 +1,54 @@
 from rest_framework import serializers
-from database.models import Flow, Trigger, AvailableTrigger, AvailableAction
+from database.models import Flow, Trigger, AvailableTrigger, AvailableAction, Action
+
+
+class AvailableTriggerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AvailableTrigger
+        fields = ['id', 'name', 'image']
+
+
+class AvailableActionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AvailableAction
+        fields = ['id', 'name', 'image']
+
+
+class ActionDetailSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Action
+        fields = ['id', 'flowId', 'actionId', 'sortingOrder', 'metadata', 'type']
+
+    def get_type(self, obj):
+        return AvailableActionSerializer(obj.actionId).data
+
+
+class TriggerDetailSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Trigger
+        fields = ['id', 'flowId', 'triggerId', 'metadata', 'type']
+    
+    def get_type(self, obj):
+        return AvailableTriggerSerializer(obj.triggerId).data
+
+
+class FlowListSerializer(serializers.ModelSerializer):
+    actions = ActionDetailSerializer(many=True, read_only=True)
+    trigger = TriggerDetailSerializer(read_only=True)
+    
+    class Meta:
+        model = Flow
+        fields = ['id', 'userId', 'triggerId', 'actions', 'trigger']
+
 
 class ActionSerializer(serializers.Serializer):
     availableActionId = serializers.CharField()
     actionMetadata = serializers.JSONField(required=False, default=dict)
+
 
 class FlowCreateSerializer(serializers.Serializer):
     availableTriggerId = serializers.CharField()
