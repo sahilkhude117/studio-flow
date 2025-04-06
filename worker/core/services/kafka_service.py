eb77beb2d568f4f5f651e567c144bc81667a3988
sahilkhude117 <sahilkhude11@gmail.com>
2025-04-06 15:39:41 +0530

    services: kafka consumer service

--- worker/core/services/kafka_service.py
+++ worker/core/services/kafka_service.py
@@ -1,16 +1,62 @@
-from kafka import KafkaConsumer
-import json
+from kafka import KafkaConsumer, TopicPartition
+from kafka.structs import OffsetAndMetadata, TopicPartition
 import logging
-import time
-from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
-class KafkaConsumerService:
-    def __init__(self):
+class KafkaService:
+    def __init__(self, topic_name="flow-events", group_id="main-worker", bootstrap_servers=["localhost:9092"]):
+        self.topic_name = topic_name
+        self.group_id = group_id
+        self.bootstrap_servers = bootstrap_servers
         self.consumer = None
-        self.topic_name = settings.KAFKA_CONSUMER_CONFIG['TOPIC_NAME']
-        self.group_id = settings.KAFKA_CONSUMER_CONFIG['GROUP_ID']
-        self.brokers = settings.KAFKA_CONSUMER_CONFIG['BROKERS']
-        self.from_beginning = settings.KAFKA_CONSUMER_CONFIG['FROM_BEGINNING']
-        self.process_delay_ms = settings.KAFKA_CONSUMER_CONFIG['PROCESS_DELAY_MS']
+
+
+    def connect(self):
+        """Connect tp Kafka"""
+
+        try: 
+            self.consumer = KafkaConsumer(
+                bootstrap_servers=self.bootstrap_servers,
+                group_id=self.group_id,
+                auto_offset_reset='earliest',
+                enable_auto_commit=False,
+                value_deserializer=lambda m: m.decode('utf-8')
+            )
+            self.consumer.subscribe([self.topic_name])
+            logger.info("Successfully connected to Kafka")
+            return True
+        except Exception as e:
+            logger.error(f'Failed to connect to Kafka: {str(e)}')
+            return False
+
+    def start(self):
+        print(f"Listening to topic: {self.topic_name}")
+
+        if not self.consumer:
+            self.connect()
+
+        try:
+            for message in self.consumer:
+                print({
+                    "partition": message.partition,
+                    "offset": message.offset,
+                    "value": message.value
+                })
+
+                # Simulate processing delay
+                import time
+                time.sleep(0.5)
+                print("processing done")
+
+                self.consumer.commit({
+                    TopicPartition(self.topic_name, message.partition): OffsetAndMetadata(message.offset + 1, None, -1)
+                })
+        except Exception as e:
+            logger.error(f"Failed to start the worker {str(e)}")
+
+    def close(self):
+        """Close the kafka consumer connection"""
+        if self.consumer:
+            self.consumer.close()
+            logger.info("Kafka consumer connection closed")
