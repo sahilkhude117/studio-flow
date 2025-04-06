830f05b8275e496511083ec06c9901f78fd13e68
sahilkhude117 <sahilkhude11@gmail.com>
2025-04-07 03:35:06 +0530

    view: available actions view

--- server/action_api/views.py
+++ server/action_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from database.models import AvailableAction
+from .serializers import AvailableActionSerializer
+
+class AvailableActionsView(APIView):
+    def get(self, request):
+        available_actions = AvailableAction.objects.all()
+        serializer = AvailableActionSerializer(available_actions, many=True)
+
+        return Response({
+            'availableActions': serializer.data
+        })
