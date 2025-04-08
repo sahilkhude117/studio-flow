b590ec23400ecaa20a8b7af3ae76d9a428139534
sahilkhude117 <sahilkhude11@gmail.com>
2025-04-08 16:11:07 +0530

    url: /api/v1/flow/:flowId

--- server/flow_api/urls.py
+++ server/flow_api/urls.py
@@ -1,10 +1,10 @@
 # flow_api/urls.py
 from django.urls import path
-from .views import FlowView, FlowDetailView, ToggleFlowStatusView, DeleteFlowView
+from .views import FlowsView, FlowView, ToggleFlowStatusView, DeleteFlowView
 
 urlpatterns = [
-    path('', FlowView.as_view(), name='flow'),
-    path('<uuid:flowId>/', FlowDetailView.as_view(), name='flow-detail'),
+    path('', FlowsView.as_view(), name='flows'),
+    path('<uuid:flowId>/', FlowView.as_view(), name='flow-detail'),
     path('<uuid:pk>/toggle/',ToggleFlowStatusView.as_view(), name="toggle-status"),
     path('<uuid:pk>/delete/',DeleteFlowView.as_view(), name='delete-flow')
 ]
