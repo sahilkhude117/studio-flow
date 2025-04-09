89eabd466ac43d5afaac9ebd66affd6883ad6421
sahilkhude117 <sahilkhude11@gmail.com>
2025-04-09 14:33:39 +0530

    ui: loader

--- frontend/src/app/flow/[flowId]/page.tsx
+++ frontend/src/app/flow/[flowId]/page.tsx
@@ -166,9 +166,8 @@ const FlowEditor = () => {
 
   if (loading && !isNew) {
     return <div>
-      <Loader2Icon className="h-8 w-8 text-primary animate-spin flex items-center justify-center h-screen w-screen" />
-    </div>
-    
+      <div>Loading.....</div>
+    </div> 
   }
 
   if (error && !isNew) {
